a5dc8c7ce88ca5968e7260fa5cf6f96207e3314c
Michaela Worschitz <00830388@edu.campus02.at>
2021-12-20 11:26:55 +0100

    Eingabeaufforderung Mensch oder KI erstellt

--- vier_gewinnt.py
+++ vier_gewinnt.py
@@ -11,3 +11,11 @@ zeile_ausgabe = 0
 while zeile_ausgabe < 6:
     print(spielfeld[zeile_ausgabe])
     zeile_ausgabe += 1
+
+print("Willkommen im Spiel '4 gewinnt'! Sie haben die Auswahl gegen einen Mensch oder eine künstliche Intelligenz zu spielen.")
+# if input("Möchten Sie gegen eine künstliche Intelligenz spielen?") == "Ja":
+#     print("richtig")
+#     pass
+
+
+print(input("Bitte wählen Sie eine Spalte für den nächsten Spielzug aus (1-7)"))
