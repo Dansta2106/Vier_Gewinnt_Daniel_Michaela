e2a58649ceacf03e0fd2772eaf199f71259cce5e
Daniel Stangl <s50809@edu.campus02.at>
2021-12-20 11:53:54 +0100

    Spielablauf erstellt
    Spielervariable initialisiert
    Definiert wie Spieler gewechselt wird
    Spielfeld nach jeder Runde ändern
    Nach jeder Runde Spielfeld ausgeben
    Eine Lösung erstellt falls eine falsche Eingabe gemacht wurde

--- vier_gewinnt.py
+++ vier_gewinnt.py
@@ -1,21 +1,43 @@
-zeile = [0, 0, 0, 0, 0, 0, 0]
+zeile1 = [0, 0, 0, 0, 0, 0, 0]
+zeile2 = [0, 0, 0, 0, 0, 0, 0]
+zeile3 = [0, 0, 0, 0, 0, 0, 0]
+zeile4 = [0, 0, 0, 0, 0, 0, 0]
+zeile5 = [0, 0, 0, 0, 0, 0, 0]
+zeile6 = [0, 0, 0, 0, 0, 0, 0]
 
-spielfeld = [zeile,
-             zeile,
-             zeile,
-             zeile,
-             zeile,
-             zeile]
+spielfeld = [zeile1,
+             zeile2,
+             zeile3,
+             zeile4,
+             zeile5,
+             zeile6]
 
-zeile_ausgabe = 0
-while zeile_ausgabe < 6:
-    print(spielfeld[zeile_ausgabe])
-    zeile_ausgabe += 1
 
 print("Willkommen im Spiel '4 gewinnt'! Sie haben die Auswahl gegen einen Mensch oder eine künstliche Intelligenz zu spielen.")
 # if input("Möchten Sie gegen eine künstliche Intelligenz spielen?") == "Ja":
 #     print("richtig")
 #     pass
 
+spieler = 1
 
-print(input("Bitte wählen Sie eine Spalte für den nächsten Spielzug aus (1-7)"))
+end = False
+while end != True:
+    fehler = False
+    zeile_ausgabe = 0
+    while zeile_ausgabe < 6:
+        print(spielfeld[zeile_ausgabe])
+        zeile_ausgabe += 1
+    eingabe = int(input("Bitte wählen Sie eine Spalte für den nächsten Spielzug aus (1-7)"))
+    if 1 <= eingabe <= 7:
+        for reihe in spielfeld[::-1]:
+            if reihe[eingabe - 1] == 0:
+                reihe[eingabe - 1] = spieler
+                break
+    else:
+        print("Fehler, falsche Eingabe")
+        fehler = True
+    if fehler == False:
+        if spieler == 1:
+            spieler -= 2
+        else:
+            spieler += 2
