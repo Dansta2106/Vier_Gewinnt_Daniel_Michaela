2c9ceba50b4f0b11261e008d00429a792926471c
Daniel Stangl <s50809@edu.campus02.at>
2021-12-20 11:10:58 +0100

    Unittests erstellt und main File geändert
    Die Datei für Unittests erstellt.
    Die Klasse Spielfeld entfernt und mit direktem Code ersetzt

--- vier_gewinnt.py
+++ vier_gewinnt.py
@@ -1,9 +1,13 @@
-class Spielfeld:
-    def __init__(self):
-        self.spielfeld = [[0, 0, 0, 0, 0, 0, 0]
-                          [0, 0, 0, 0, 0, 0, 0]
-                          [0, 0, 0, 0, 0, 0, 0]
-                          [0, 0, 0, 0, 0, 0, 0]
-                          [0, 0, 0, 0, 0, 0, 0]
-                          [0, 0, 0, 0, 0, 0, 0]]
+zeile = [0, 0, 0, 0, 0, 0, 0]
 
+spielfeld = [zeile,
+             zeile,
+             zeile,
+             zeile,
+             zeile,
+             zeile]
+
+zeile_ausgabe = 0
+while zeile_ausgabe < 6:
+    print(spielfeld[zeile_ausgabe])
+    zeile_ausgabe += 1
---
+++ vier_gewinnt_test.py
@@ -0,0 +1,11 @@
+import unittest
+from vier_gewinnt import *
+
+
+class VierGewinntTestCases(unittest.TestCase):
+    pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
