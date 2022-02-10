108e3de1d7e3014fbef669346aacf1391821d112
Daniel Stangl <s50809@edu.campus02.at>
2022-02-10 10:29:26 +0100

    An neue Dateistruktur angepasst

--- tests/vier_gewinnt_test.py
+++ tests/vier_gewinnt_test.py
@@ -1,14 +1,13 @@
 import unittest
 
-import vier_gewinnt
-from vier_gewinnt import *
+import vier_gewinnt_daniel_michaela
 
 
 class VierGewinntTestCases(unittest.TestCase):
     def setUp(self) -> None:
-        self.spieler = vier_gewinnt.spieler
-        self.spielerAusgabetest = vier_gewinnt.Spieler.spielerAusgabe(self.spieler)
-        self.spielerAusgabetest2 = vier_gewinnt.Spieler.spielerAusgabe()
+        self.spieler = vier_gewinnt_daniel_michaela.vier_gewinnt.spieler
+        self.spielerAusgabetest = vier_gewinnt_daniel_michaela.Spieler.spielerAusgabe(self.spieler)
+        self.spielerAusgabetest2 = vier_gewinnt_daniel_michaela.Spieler.spielerAusgabe()
 
     def test_SpielerAusgabe(self):
         self.assertEqual(self.spielerAusgabetest, "Spieler 1 ist dran")
